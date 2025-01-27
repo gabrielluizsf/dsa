254788e25f36d5bb19b71057afa4dfafa8f85aca
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-01-27 10:09:32 -0300

    binary search

--- arrays.go
+++ arrays.go
@@ -1,16 +1,34 @@
 package dsa
 
+func BinarySearch(nums []int, n int) (steps, index int) {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		steps++
+		middle := (low + high) / 2
+		if nums[middle] == n {
+			index = middle
+			return
+		} else if nums[middle] < n {
+			low = middle + 1
+		} else {
+			high = middle
+		}
+	}
+	index = -1
+	return
+}
+
 func ReverseWords(s string) string {
 	chars := []rune(s)
 	start := 0
-	
+
 	for i := 0; i <= len(chars); i++ {
 		if i == len(chars) || chars[i] == ' ' {
 			reverse(chars, start, i-1)
 			start = i + 1
 		}
 	}
-	
+
 	return string(chars)
 }
 
--- arrays_test.go
+++ arrays_test.go
@@ -6,14 +6,42 @@ import (
 	"github.com/i9si-sistemas/assert"
 )
 
+func TestBinarySearch(t *testing.T) {
+	arr := []int{1}
+	steps, index := BinarySearch(arr, 1)
+	assert.Equal(t, steps, 1)
+	assert.Equal(t, index, 0)
+
+	arr = []int{1, 2, 3, 4, 5}
+	steps, index = BinarySearch(arr, 3)
+	assert.Equal(t, steps, 1)
+	assert.Equal(t, index, 2)
+
+	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	steps, index = BinarySearch(arr, 3)
+	assert.Equal(t, steps, 2)
+	assert.Equal(t, index, 2)
+
+	
+	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	steps, index = BinarySearch(arr, 3)
+	assert.Equal(t, steps, 3)
+	assert.Equal(t, index, 2)
+
+	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40}
+	steps, index = BinarySearch(arr, 3)
+	assert.Equal(t, steps, 4)
+	assert.Equal(t, index, 2)
+}
+
 func TestReverseWords(t *testing.T) {
 	words := "Hello World"
 	result := ReverseWords(words)
 	expected := "olleH dlroW"
 	assert.Equal(t, result, expected)
-	
+
 	words = "Let's take LeetCode contest"
-	result  = ReverseWords(words)
+	result = ReverseWords(words)
 	expected = "s'teL ekat edoCteeL tsetnoc"
 	assert.Equal(t, result, expected)
 
