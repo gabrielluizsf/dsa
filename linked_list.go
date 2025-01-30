62b2c3a42b337994a3954863d9141e7c777797bd
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-01-30 10:51:39 -0300

    Find cycles in the linked list

--- linked_list.go
+++ linked_list.go
@@ -124,3 +124,18 @@ func MiddleNode(head *ListNode) *ListNode {
 	}
 	return head
 }
+
+func HasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil{
+        return false
+    }
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
--- linked_list_test.go
+++ linked_list_test.go
@@ -47,7 +47,6 @@ func TestReverseLinkedList(t *testing.T) {
 	assert.Equal(t, ll.String(), "5 -> 4 -> 3 -> 2 -> 1 -> nil")
 }
 
-
 func TestMiddleOfTheLinkedList(t *testing.T) {
 	ll := &ListNode{Val: 1}
 	ll.Add(2)
@@ -56,4 +55,27 @@ func TestMiddleOfTheLinkedList(t *testing.T) {
 	ll.Add(5)
 	ll = MiddleNode(ll)
 	assert.Equal(t, ll.Val, 3)
+}
+
+func TestLinkedListCycle(t *testing.T) {
+    var head *ListNode
+    assert.False(t, HasCycle(head))
+
+    head = &ListNode{Val: 1}
+    assert.False(t, HasCycle(head))
+
+    node1 := &ListNode{Val: 1}
+    node2 := &ListNode{Val: 2}
+    node3 := &ListNode{Val: 3}
+    node1.Next = node2
+    node2.Next = node3
+    node3.Next = node1 
+    assert.True(t, HasCycle(node1))
+
+    node1 = &ListNode{Val: 1}
+    node2 = &ListNode{Val: 2}
+    node3 = &ListNode{Val: 3}
+    node1.Next = node2
+    node2.Next = node3
+    assert.False(t, HasCycle(node1))
 }
