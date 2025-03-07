d20ba4b3396acb12a26c8625f859831286eb64ad
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-03-07 09:59:23 -0300

    implement NumberOfSteps function to calculate steps to reduce a number to zero

--- binary_operations.go
+++ binary_operations.go
@@ -35,3 +35,17 @@ func MissingNumber(nums []int) int {
 	}
 	return x
 }
+
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+func NumberOfSteps(num int) int {
+	steps := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num /= 2
+		} else {
+			num--
+		}
+		steps++
+	}
+	return steps
+}
